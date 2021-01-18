6c53c0ebcd71cd03c32781dc7933749bf592fc10
cryinggriaffe <490949304@qq.com>
2021-01-18 22:24:55 +0800

    gateway

--- network/src/com/company/service/ConfigService.java
+++ network/src/com/company/service/ConfigService.java
@@ -1,8 +1,10 @@
 package com.company.service;
 
+import java.io.*;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
+import java.nio.charset.Charset;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -17,6 +19,7 @@ public class ConfigService {
             String IP = addr.getHostAddress().toString();
             ip_and_mac.put("ip",IP);
             ip_and_mac.put("mac",getLocalMac(addr));
+            ip_and_mac.put("res",getGateway(IP));
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -44,6 +47,41 @@ public class ConfigService {
         }
         //System.out.println("本机MAC地址:" + sb.toString().toUpperCase());
         return sb.toString().toUpperCase();
+    }
+
+    public static String  getGateway(String ip){
+
+        StringBuffer result = new StringBuffer();
+
+        try {
+            Process process = Runtime.getRuntime().exec("ipconfig");
+            InputStreamReader inputStreamReader = new InputStreamReader(process.getInputStream(), Charset.forName("GBK"));
+            //BufferedReader reader = new BufferedReader(inputStreamReader);
+            LineNumberReader reader = new LineNumberReader(inputStreamReader);
+            String line = null;
+
+            int lines = 0;
+            while((line = reader.readLine()) != null){
+                if (line.contains(ip)){
+                    lines = reader.getLineNumber();
+                    System.out.println("lines:"+lines);
+                    continue;
+                }
+
+                if (lines < reader.getLineNumber() && reader.getLineNumber() < lines + 3){
+                    System.out.println(reader.getLineNumber());
+                    //result.append(line);
+                }
+            }
+            System.out.println(result.toString());
+            reader.close();
+            inputStreamReader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
     }
 
 }
